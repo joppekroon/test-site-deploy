1e797db9d8899449f26c904237288e5e01b0aba2
joppekroon <joppekroon@users.noreply.github.com>
2022-03-09 10:30:41 +0000

    deploy: 5f3fbf60d0f648b6272f5632e2fb9db7ae641ce6

--- docs/components/another-element.js
+++ docs/components/another-element.js
@@ -1,2 +1,2 @@
-import{s as e,r as t,$ as l}from"./lit-element-65efed9f.js";class r extends e{static styles=t`p { color: red }`;static properties={yell:{type:String}};constructor(){super(),this.yell="Fadaladaladala"}render(){return l`<p>Bump it, ${this.yell}!</p>`}}customElements.define("another-element",r);export{r as AnotherElement};
+import{s as e,r as t,$ as r}from"./lit-element-65efed9f.js";class l extends e{static styles=t`p { color: red }`;static properties={yell:{type:String}};constructor(){super(),this.yell="Fadaladaladala"}render(){return r`<p>Scream it, <b>${this.yell}!</b></p>`}}customElements.define("another-element",l);export{l as AnotherElement};
 //# sourceMappingURL=another-element.js.map
